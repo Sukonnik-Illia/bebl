39e3877845bbb91450da9d3fdd132e45d1b7a203
Illia Sukonnik <illia.sukonnik@gmail.com>
2024-04-01 21:50:43 +0300

    forgotten imports

--- bebl/config.py
+++ bebl/config.py
@@ -1,3 +1,5 @@
+from typing import Any, Dict, Optional
+
 from pydantic import PostgresDsn, validator
 from pydantic_settings import BaseSettings
 
--- bebl/main.py
+++ bebl/main.py
@@ -1,5 +1,3 @@
-from typing import Any, Dict, Optional, Union
-
 from flask import Flask
 
 from bebl import exts
